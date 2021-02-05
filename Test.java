ce7e667ff02e3741037b63bc86c2e327676ed702
arpit1992-github <arpitmalviya.eng@gmail.com>
2021-02-05 17:31:59 +0530

    msg

--- Test.java
+++ Test.java
@@ -1,6 +1,10 @@
 class Test{
     public private static Type name() {
+<<<<<<< Updated upstream
        System.out.println("My name is arpit");
 
+=======
+       System.out.println("my name is shubham");
+>>>>>>> Stashed changes
     }
 }
